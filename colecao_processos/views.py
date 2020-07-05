f2015eab30ae35540bc9994649819c5cb7650bd8
joao.monte <joao.monte@gprt.ufpe.br>
2020-07-04 22:24:23 -0300

    Adicionado um exemplo de view

--- colecao_processos/views.py
+++ colecao_processos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+def processos(request):
+    data = {"oi": "2"}
+    return JsonResponse(data)
