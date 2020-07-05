b5d4402ecaf48258d65e1b6433037964fc3c13e9
joao.monte <joao.monte@gprt.ufpe.br>
2020-07-04 22:47:12 -0300

    Atualizada a função processos para listar todos os processos

--- colecao_processos/views.py
+++ colecao_processos/views.py
@@ -1,7 +1,16 @@
 from django.http import JsonResponse
-
+from django.apps import apps
+from colecao_processos.models import Processo
 
 # Create your views here.
 def processos(request):
-    data = {"oi": "2"}
-    return JsonResponse(data)
+    models = apps.get_app_config('colecao_processos').get_models()
+    output_json = {"Processos": []}
+    for processo in Processo.objects.all():
+        processo_json = {
+            "pasta": processo.pasta,
+            "comarca": processo.comarca,
+            "uf": processo.uf
+        }
+        output_json["Processos"].append(processo_json)
+    return JsonResponse(output_json)
