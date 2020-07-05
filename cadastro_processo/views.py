88b656398924b0f4917c021374a31ad5ec7fa1bf
joao.monte <joao.monte@gprt.ufpe.br>
2020-07-05 20:45:09 -0300

    Implementado o método que faz o processamento dos csvs e cria instancias do modelo Processo para cada linha da planilha dada como upload

--- cadastro_processo/views.py
+++ cadastro_processo/views.py
@@ -1,23 +1,29 @@
 import csv
 from django.shortcuts import render
+from django.apps import apps
 from .forms import UploadPlanilha
 from .models import Planilha
-#from cadastramento_processos.colecao_processos.models import Processo
 
 # Create your views here.
-
 def upload(request):
     form = UploadPlanilha()
     if request.method == 'POST':
-        form = UploadPlanilha(request.POST, request.FILES)  # Do not forget to add: request.FILES
+        form = UploadPlanilha(request.POST, request.FILES)
         if form.is_valid():
-            print(request.FILES)
-            print(request.FILES['planilha'])
-            #Criar modelo 
-            # Do something with our files or simply save them
-            # if saved, our files would be located in media/ folder under the project's base folder
-            #form.save()
+            #Chama a função para processamento dos CSVs 
+            processaCsv(request.FILES['planilha'])
     return render(request, 'uploadScreen.html', locals())
 
-
-
+def processaCsv(filename):
+    #Processa o csv e cria modelos do tipo Processo com as informações contidas
+    #na planilha
+    csvfile = filename.read().decode("utf-8") 
+    #Carrega dinamicamente o modelo Processo
+    Processo = apps.get_model('colecao_processos', 'Processo')
+    for line in csvfile.split('\n'):
+        if line:
+            pasta, comarca, uf = line.split(';') 
+            uf = uf.replace("\r", "")
+            novoProcesso = Processo(pasta=pasta, comarca=comarca, uf=uf)
+            novoProcesso.save()
+    
