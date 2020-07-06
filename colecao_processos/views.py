1c6dbcd9f238f7a22dbb2a4fb777307c9daa9042
joao.monte <joao.monte@gprt.ufpe.br>
2020-07-05 21:32:33 -0300

    Removido código não utilizado mais

--- colecao_processos/views.py
+++ colecao_processos/views.py
@@ -1,10 +1,8 @@
 from django.http import JsonResponse
-from django.apps import apps
 from colecao_processos.models import Processo
 
 # Create your views here.
 def listaProcessos(request):
-    models = apps.get_app_config('colecao_processos').get_models()
     output_json = {"Processos": []}
     for processo in Processo.objects.all():
         processo_json = {
